af50d9fc6ddcc36720a794ead7309762acc127c9
pablomartan <pablomartan@kolabnow.com>
2023-04-13 13:08:52 +0200

    feat: show time left in Timer component

--- src/js/components/Timer.js
+++ src/js/components/Timer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export const Timer = props => {
+
   return(
     <div id="timer">
       <div id="timer-label"></div>
-      <div id="time-left"></div>
+      <div id="time-left">{props.timeLeft}</div>
     </div>
   )
 };
