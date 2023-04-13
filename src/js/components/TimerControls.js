9f5289c822eb9f3cc6f4194139c0db8933a56ddb
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:57:35 +0200

    feat: add reset function to TimerControls component

--- src/js/components/TimerControls.js
+++ src/js/components/TimerControls.js
@@ -5,11 +5,15 @@ const play = <i className="fa-solid fa-play"></i>;
 const pause = <i className="fa-solid fa-pause"></i>;
 
 export const TimerControls = props => {
+  const reset = () => {
+    props.setBr(5);
+    props.setSe(25);
+  };
 
   return(
     <div id="timer-controls">
       <div id="start_stop">{started? play : pause}</div>
-      <div id="reset"><i className="fa-solid fa-repeat"></i></div>
+      <div id="reset" onClick={() => reset()}><i className="fa-solid fa-repeat"></i></div>
     </div>
   );
 };
