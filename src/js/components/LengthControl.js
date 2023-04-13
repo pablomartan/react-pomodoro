5bffb5bf4b9c1614f280ac2d8838c2225afc2bb4
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:31:08 +0200

    feat: add btn bootstrap class to length control divs

--- src/js/components/LengthControl.js
+++ src/js/components/LengthControl.js
@@ -21,16 +21,16 @@ export const LengthControl = props => {
     <div id="length-control" className="row">
         <div id="break-control" className="row col text-center">
           <div id="break-label">Break</div>
-          <div id="break-decrement" className="col" onClick={() => brDec()}><i class="fa-solid fa-arrow-down"></i></div>
+          <div id="break-decrement" className="col btn" onClick={() => brDec()}><i class="fa-solid fa-arrow-down"></i></div>
           <div id="break-lenght" className="col">{props.brLength}</div>
-          <div id="break-increment" className="col" onClick={() => brInc()}><i class="fa-solid fa-arrow-up"></i></div>
+          <div id="break-increment" className="col btn" onClick={() => brInc()}><i class="fa-solid fa-arrow-up"></i></div>
         </div>
 
         <div id="session-control" className="row col text-center">
           <div id="session-label">Session</div>
-          <div id="session-decrement" className="col" onClick={() => seDec()}><i class="fa-solid fa-arrow-down"></i></div>
+          <div id="session-decrement" className="col btn" onClick={() => seDec()}><i class="fa-solid fa-arrow-down"></i></div>
           <div id="session-length" className="col">{props.seLength}</div>
-          <div id="session-increment" className="col" onClick={() => seInc()}><i class="fa-solid fa-arrow-up"></i></div>
+          <div id="session-increment" className="col btn" onClick={() => seInc()}><i class="fa-solid fa-arrow-up"></i></div>
         </div>
     </div>
   )
