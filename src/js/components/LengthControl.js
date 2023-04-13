9c268d7cce0c092a572fa399a756ce40e7a1e366
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:09:38 +0200

    feat: add labels to break and session label divs in LengthControl components

--- src/js/components/LengthControl.js
+++ src/js/components/LengthControl.js
@@ -4,14 +4,14 @@ export const LengthControl = props => {
   return(
     <div id="length-control">
         <div id="break-control">
-          <div id="break-label"></div>
+          <div id="break-label">Break</div>
           <div id="break-decrement"></div>
           <div id="break-lenght"></div>
           <div id="break-increment"></div>
         </div>
 
         <div id="session-control">
-          <div id="session-label"></div>
+          <div id="session-label">Session</div>
           <div id="session-decrement"></div>
           <div id="session-length"></div>
           <div id="session-increment"></div>
