4c47da0c894df13175cfa39744d89633c5ea864f
pablomartan <pablomartan@kolabnow.com>
2023-04-13 11:58:22 +0200

    feat: create App Component

---
+++ src/js/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export const App = () => {
+
+  return(
+    <div id="app" className="w-100">
+      <h1 className="text-center">Work + break timer</h1>
+    </div>
+  );
+};
