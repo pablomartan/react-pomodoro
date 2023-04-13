b97aafe06a56d76fbdaf103ae0f5fc439c1f75a7
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:32:41 +0200

    feat: add featureless Timer component

---
+++ src/js/components/Timer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Timer = props => {
+  return(
+    <div id="timer">
+      <div id="timer-label"></div>
+      <div id="time-left"></div>
+    </div>
+  )
+};
