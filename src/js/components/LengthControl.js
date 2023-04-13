b06dfdd58b7fb9162d2293489d49d1b2e9c5c4eb
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:10:25 +0200

    feat: set break and session length from props in LengthControl component

--- src/js/components/LengthControl.js
+++ src/js/components/LengthControl.js
@@ -6,14 +6,14 @@ export const LengthControl = props => {
         <div id="break-control">
           <div id="break-label">Break</div>
           <div id="break-decrement"></div>
-          <div id="break-lenght"></div>
+          <div id="break-lenght">{props.brLength}</div>
           <div id="break-increment"></div>
         </div>
 
         <div id="session-control">
           <div id="session-label">Session</div>
           <div id="session-decrement"></div>
-          <div id="session-length"></div>
+          <div id="session-length">{props.seLength}</div>
           <div id="session-increment"></div>
         </div>
     </div>
