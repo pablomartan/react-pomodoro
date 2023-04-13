6ef7cb9285f3e651c9a7e1e3f60089385a1279fc
pablomartan <pablomartan@kolabnow.com>
2023-04-13 13:10:20 +0200

    feat: add run counter to Timer component and display label conditionaly on number of runs

--- src/js/components/Timer.js
+++ src/js/components/Timer.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Timer = props => {
+  const [ runs, setRuns ] = useState(0);
+  const label = runs % 2 == 0 ? 'Session' : 'Break';
 
   return(
     <div id="timer">
-      <div id="timer-label"></div>
+      <div id="timer-label">{label}</div>
       <div id="time-left">{props.timeLeft}</div>
     </div>
   )
