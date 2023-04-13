85f3e3fe5e968f6ec3cf3f5d3238d7df444c6171
pablomartan <pablomartan@kolabnow.com>
2023-04-13 12:48:36 +0200

    feat: import TimerControls component into App

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { LengthControl } from './components/LengthControl';
 import { Timer } from './components/Timer';
+import { TimerControls } from './components/TimerControls';
 
 export const App = () => {
   const [ breakLength, setBreakLength ] = useState(5);
@@ -11,6 +12,7 @@ export const App = () => {
       <h1 className="text-center">Work + break timer</h1>
       <LengthControl brLength={breakLength} seLength={sessionLength} setBr={setBreakLength} setSe={setSessionLength} />
       <Timer />
+      <TimerControls />
     </div>
   );
 };
