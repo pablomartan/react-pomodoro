35ee4383798ec2a152d01d13ed0f202075f460eb
pablomartan <pablomartan@kolabnow.com>
2023-04-13 13:05:48 +0200

    feat: add timeLeft state and pass it to Timer

--- src/js/App.js
+++ src/js/App.js
@@ -6,12 +6,13 @@ import { TimerControls } from './components/TimerControls';
 export const App = () => {
   const [ breakLength, setBreakLength ] = useState(5);
   const [ sessionLength, setSessionLength ] = useState(25);
+  const [ timeLeft, setTimeLeft ] = useState(`${sessionLength}:00`);
 
   return(
     <div id="app" className="w-100">
       <h1 className="text-center">Work + break timer</h1>
       <LengthControl brLength={breakLength} seLength={sessionLength} setBr={setBreakLength} setSe={setSessionLength} />
-      <Timer />
+      <Timer timeLeft={timeLeft} brLength={breakLength} seLength={sessionLength} />
       <TimerControls setBr={setBreakLength} setSe={setSessionLength} />
     </div>
   );
